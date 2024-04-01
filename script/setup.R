bc3bf598fe3c776ca3338bb50c3fa37d49d0aa5f
Kubale <jkubale@UMICH.EDU>
2024-04-01 12:54:58 -0400

    initial commit and update gitignore

---
+++ script/setup.R
@@ -0,0 +1,7 @@
+## To do:
+# import Wisconsin data
+# import RUCA codes
+# merge Wisconsin data with RUCA codes
+# calculate mean and variance of COVID events over time by county
+# calculate mean and variance of COVID events over time by census tract
+# calculate 
